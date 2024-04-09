a499479644b67fda31eaa7694fc741c00489e6c2
Sajal Biswas <sajalphys@gmail.com>
2024-04-09 08:47:43 +0530

    QR Code generator for any text

---
+++ qr.py
@@ -0,0 +1,23 @@
+import qrcode
+from PIL import Image
+
+# Create a QRCode object with customizable options:
+qr = qrcode.QRCode(
+    version=1,  # Adjust version for data capacity (1-40)
+    error_correction=qrcode.constants.ERROR_CORRECT_H,  # Set error correction level (L, M, Q, H)
+    box_size=20,  # Set module size in pixels
+    border=4,  # Set border width
+)
+
+# Add the data to be encoded:
+qr.add_data("https://sites.google.com/view/sajalphys")  # Replace with your desired data
+
+# Generate the QR code:
+qr.make(fit=True)  # Efficiently utilize space
+
+# Create a PIL image with preferred colors:
+img = qr.make_image(fill_color="black", back_color="white")  # Customize colors as needed
+
+# Save the QR code image:
+img.save("mysite.png")  # Set desired filename
+
